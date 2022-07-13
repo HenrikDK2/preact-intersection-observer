3334ea992407caacda5f337f8a69371d6ec4a7fb
HenrikDK2 <henrik265@outlook.dk>
2022-07-13 22:14:14 +0200

    Fixes, and use generic instead of predefined type

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { createRef, RefObject } from "preact";
-import { useEffect, useState } from "preact/hooks";
+import { Ref } from "preact";
+import { useEffect, useRef, useState } from "preact/hooks";
 
 interface ObserverOptions {
   rootMargin?: string;
@@ -7,28 +7,40 @@ interface ObserverOptions {
   triggerOnce?: boolean;
 }
 
-type ReturnArr = [ref: RefObject<HTMLElement>, inView: boolean];
 const isBrowser = typeof window !== "undefined";
 
-export default function (options: ObserverOptions): ReturnArr {
-  const ref: RefObject<HTMLElement> = createRef();
-  const init: RefObject<boolean> = createRef();
+export const useObserver = <T extends HTMLElement>(
+  options?: ObserverOptions
+): [ref: Ref<T>, inView: boolean] => {
   const [inView, setInView] = useState<boolean>(false);
+  const observer = useRef<IntersectionObserver>();
+  const ref = useRef<T>(null);
+  const init = useRef<boolean>(false);
 
   if (isBrowser) {
     const observerCallback = (entries: any) =>
-      entries[0].isIntersecting === !inView && setInView(!inView);
-    const observer = new IntersectionObserver(observerCallback, { ...options, root: ref.current });
+      setInView(entries[0].isIntersecting);
+
+    if (!observer.current) {
+      observer.current = new IntersectionObserver(observerCallback, {
+        ...options,
+        root: ref.current,
+      });
+    }
 
     useEffect(() => {
-      if (ref?.current && !init.current) {
-        observer.observe(ref.current);
-        //@ts-ignore
-        init.current = true;
+      if (observer.current && ref.current) {
+        if (ref?.current && !init.current) {
+          observer.current.observe(ref.current);
+          init.current = true;
+        }
+
+        if (options?.triggerOnce && init.current && inView === true) {
+          observer.current.unobserve(ref.current);
+        }
       }
-      if (inView && options?.triggerOnce) observer.unobserve(ref.current!);
-    }, [ref, inView]);
+    }, [ref, inView, ref.current]);
   }
 
   return [ref, inView];
-}
+};
