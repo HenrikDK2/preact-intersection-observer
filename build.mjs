7628c9fa44a3cd67b684c576a20a56f9dc385f64
HenrikDK2 <henrik265@outlook.dk>
2023-01-21 22:48:21 +0100

    eslint on build

--- build.mjs
+++ build.mjs
@@ -1,16 +1,20 @@
 import { execSync } from "child_process";
 import { gzipSizeFromFileSync } from "gzip-size";
 
-// Delete dist folder, and build project
-execSync("shx rm -rf ./dist .cache && tsc");
+try {
+  // Delete dist folder, and build project
+  execSync("shx rm -rf ./dist .cache && tsc");
 
-// Prettier
-execSync("npx prettier --write .");
+  // Prettier
+  execSync("npx prettier --write .");
 
-// Optimize files
-const files = ["dist/index.js"];
+  // Optimize files
+  const files = ["dist/index.js"];
 
-for (const file of files) {
-  execSync(`terser -o ${file} --compress --mangle -- ${file}`);
-  console.log(`Optimized ${file} - 0.${gzipSizeFromFileSync(file)}KB`);
+  for (const file of files) {
+    execSync(`terser -o ${file} --compress --mangle -- ${file}`);
+    console.log(`Optimized ${file} - 0.${gzipSizeFromFileSync(file)}KB`);
+  }
+} catch (error) {
+  console.log(error);
 }
