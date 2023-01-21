08d4e10ef196bea7239c82992770c93a3ce60015
HenrikDK2 <henrik265@outlook.dk>
2023-01-21 19:49:59 +0100

    Optimize with terser script

---
+++ optimize.cjs
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { execSync } = require("child_process");
+
+const files = ["dist/index.js", "dist/ViewportObserver.js"];
+
+try {
+  console.log("Optimizing...");
+  for (const file of files) {
+    execSync(`terser -o ${file} --compress --mangle -- ${file}`);
+    console.log(`Optimized ${file}`);
+  }
+} catch (err) {
+  console.error(err);
+}
